e5cfce24341dee1484a98bfdeb99be7bad6ca472
Decartes992 <68981451+Decartes992@users.noreply.github.com>
2024-08-09 01:32:47 -0300

    Shifted assignment of isbranch variable to decode instead of execution stage

--- XM23p-Pipelined--Register-Instructions/branch_instructions.c
+++ XM23p-Pipelined--Register-Instructions/branch_instructions.c
@@ -14,62 +14,52 @@ File Purpose: This file contains the implementation of the branch instructions.
 
 // Implementations of branch instructions
 void execute_BL(void) {
-    isBranch = TRUE;
     *LR = *PC;
     *PC += offset_BL;
 }
 
 void execute_BEQBZ(void) {
-    isBranch = TRUE;
-
     if (psw.ZF == TRUE) {
         *PC += offset_BR;
     }
 }
 
 void execute_BNEBNZ(void) {
-    isBranch = TRUE;
     if (psw.ZF == FALSE) {
         *PC += offset_BR;
     }
 }
 
 void execute_BCBHS(void) {
-    isBranch = TRUE;
     if (psw.CF == TRUE) {
         *PC += offset_BR;
     }
 }
 
 void execute_BNCBLO(void) {
-    isBranch = TRUE;
     if (psw.CF == FALSE) {
         *PC += offset_BR;
     }
 }
 
 void execute_BN(void) {
-    isBranch = TRUE;
     if (psw.SF == TRUE) {
         *PC += offset_BR;
     }
 }
 
 void execute_BGE(void) {
-    isBranch = TRUE;
     if (psw.SF == psw.OF) {
         *PC += offset_BR;
     }
 }
 
 void execute_BLT(void) {
-    isBranch = TRUE;
     if (psw.SF != psw.OF) {
         *PC += offset_BR;
     }
 }
 
 void execute_BRA(void) {
-    isBranch = TRUE;
     *PC += offset_BR;
 }
